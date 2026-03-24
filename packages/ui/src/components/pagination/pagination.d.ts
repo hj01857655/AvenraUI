eb62c8010c72018ee12e88c7cdfd884e6d026b9d
hj01857655 <1292548381@qq.com>
2026-03-24 13:24:58 +0800

    chore(repo): remove generated ui artifact files

--- packages/ui/src/components/pagination/pagination.d.ts
+++
@@ -1,9 +0,0 @@
-import type { HTMLAttributes } from 'react';
-export interface PaginationProps extends HTMLAttributes<HTMLElement> {
-    ariaLabel?: string;
-    currentPage: number;
-    onPageChange: (page: number) => void;
-    siblingCount?: number;
-    totalPages: number;
-}
-export declare function Pagination({ ariaLabel, className, currentPage, onPageChange, siblingCount, totalPages, ...props }: PaginationProps): import("react/jsx-runtime").JSX.Element;
